690ad8ddd0b047815200985f6ab3cbf080e4e073
Charbel Kaed <charbelk@google.com>
2026-02-08 07:38:27 -0500

    chore: bump versionCode to 30

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
         applicationId = "com.healthio"
         minSdk = 26
         targetSdk = 35
-        versionCode = 29
+        versionCode = 30
         versionName = "1.1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
