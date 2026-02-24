497bcfa9575a62cd47b73517ea849731847df111
Charbel Kaed <charbelk@google.com>
2026-02-23 21:33:21 -0500

    chore: bump version code to 38

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
         applicationId = "com.healthio"
         minSdk = 26
         targetSdk = 35
-        versionCode = 37
+        versionCode = 38
         versionName = "1.1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
